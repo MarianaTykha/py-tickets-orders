1483cc78668b7af4a6f3496ab6a7b880ec133a0f
mariana-tykha <marianatikhaya@gmail.com>
2025-03-30 20:06:48 +0300

    Refactor views, serializers, and models for better functionality and organization

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,21 +1,20 @@
 from django.urls import path, include
 from rest_framework import routers
-
 from cinema.views import (
     GenreViewSet,
     ActorViewSet,
     CinemaHallViewSet,
     MovieViewSet,
-    MovieSessionViewSet,
+    MovieSessionViewSet, OrderViewSet,
 )
 
 router = routers.DefaultRouter()
-router.register("genres", GenreViewSet)
-router.register("actors", ActorViewSet)
+@@ -15,7 +15,8 @@
 router.register("cinema_halls", CinemaHallViewSet)
 router.register("movies", MovieViewSet)
 router.register("movie_sessions", MovieSessionViewSet)
+router.register("orders", OrderViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = router.urls
 
 app_name = "cinema"
